c9ce8b5cd8793abd5f5c012bc3c86efd4384cf3b
plemanach <plemanach@hotmail.com>
2016-11-15 20:22:08 +0000

    Adding Segmented sieve algo.

---
+++ PrimesTests/SegmentedSieveAlgoTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Primes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primes.Tests
+{
+    [TestClass()]
+    public class SegmentedSieveAlgoTests
+    {
+        [TestMethod()]
+        public void FindPrimesLimitTest()
+        {
+            var primesAlgo = new SegmentedSieveAlgo();
+            var expectedPrimes = new long[] { 2, 3, 5, 7 };
+            var primesResult = primesAlgo.FindPrimesLimit(10).ToArray();
+            CollectionAssert.AreEqual(expectedPrimes, primesResult);
+        }
+    }
+}
