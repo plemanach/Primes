4c21e81ef0c49bd289677500751a50704e1af33b
plemanach <plemanach@hotmail.com>
2016-11-15 21:43:20 +0000

    Adding Console

---
+++ PrimesTools/PrimesTools.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimesTools
+{
+    public class PrimesTools
+    {
+        public bool TryValidateArguments(string[] args, out long numberOfPrime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
