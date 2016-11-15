74ac2c3c1f4f2d405ffd97c7f2ea378d05586941
plemanach <plemanach@hotmail.com>
2016-11-15 18:53:18 +0000

    Add Check Limit test.

--- PrimesTests/SimpleSieveAlgoTests.cs
+++ PrimesTests/SimpleSieveAlgoTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Primes;
+using System;
 using System.Linq;
 
 namespace Primes.Tests
@@ -14,6 +15,14 @@ namespace Primes.Tests
             var expectedPrimes = new long[] { 2, 3, 5, 7 };
             var primesResult = primesAlgo.FindPrimesLimit(10).ToArray();
             CollectionAssert.AreEqual(expectedPrimes, primesResult);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod()]
+        public void FindPrimesLimitWithTooBigNumberThrowArgumentExeptionTest()
+        {
+            var primesAlgo = new SimpleSieveAlgo();
+            var primesResult = primesAlgo.FindPrimesLimit(int.MaxValue).ToArray();
         }
     }
 }
