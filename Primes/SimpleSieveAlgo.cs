ebce698e406e2ae4e14e57f5083428d3187f6cda
plemanach <plemanach@hotmail.com>
2016-11-15 18:42:13 +0000

    Create a simple sieve of eratosthenes algo

---
+++ Primes/SimpleSieveAlgo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Primes
+{
+    public class SimpleSieveAlgo
+    {
+        public IEnumerable<long> FindPrimesLimit(int limit)
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}
---
+++ PrimesTests/SimpleSieveAlgoTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Primes;
+
+namespace Primes.Tests
+{
+    [TestClass()]
+    public class SimpleSieveAlgoTests
+    {
+        [TestMethod()]
+        public void FindPrimesLimitTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
